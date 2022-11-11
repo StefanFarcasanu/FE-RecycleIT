f63f698b18ff456011d1786bfddfcd722595e6df
Livius Habuc <livius.habuc@gmail.com>
2022-11-11 11:59:14 +0200

    Installed npm modules, added angular material and bootstrap and set up requests-list component

--- frontend-recycle-it/src/app/app-routing.module.ts
+++ frontend-recycle-it/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RequestsListComponent} from "./components/main-view/requests-list/requests-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "requests-list",
+    component: RequestsListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RoutingComponents = [RequestsListComponent];
--- frontend-recycle-it/src/app/app.module.ts
+++ frontend-recycle-it/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, RoutingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RequestsListComponent } from './components/main-view/requests-list/requests-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponents,
+    RequestsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
