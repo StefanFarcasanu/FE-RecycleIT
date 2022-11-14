3ee2a5695fced55068e04a4997cece71373c386c
Paula Gal <galpaullla@gmail.com>
2022-11-14 09:57:42 +0200

    login ui

--- frontend-recycle-it/src/app/app-routing.module.ts
+++ frontend-recycle-it/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {MainViewComponent} from "./main-view/main-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main-view',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'main-view',
+    component: MainViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
+export const RoutingComponents = [LoginComponent, MainViewComponent]
