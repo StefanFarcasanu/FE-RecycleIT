3c4e31683c2404e346a3462dd8d801f681e71ad2
Livius Habuc <livius.habuc@gmail.com>
2023-01-09 19:43:26 +0200

    Added county's abbreviation text on hover

--- frontend-recycle-it/src/app/components/main-view/national-statistics/national-statistics.component.ts
+++ frontend-recycle-it/src/app/components/main-view/national-statistics/national-statistics.component.ts
@@ -9,7 +9,24 @@ export class NationalStatisticsComponent implements OnInit {
 
   constructor() { }
 
+  addCountiesAbbreviations() {
+    let svgPaths = document.querySelectorAll("path");
+
+    svgPaths.forEach(path => {
+      let svgTextElement = document.createElementNS("http://www.w3.org/2000/svg", "text");
+      let smallestBox = path.getBBox();
+      svgTextElement.setAttribute("transform",
+        "translate(" + (smallestBox.x + smallestBox.width/2) + " " + (smallestBox.y + smallestBox.height/2) + ")");
+      svgTextElement.textContent = path.getAttribute("name");
+      svgTextElement.setAttribute("fill", "white");
+      svgTextElement.setAttribute("font-size", "14");
+      svgTextElement.setAttribute("pointer-events", "none");
+      path.parentNode!.insertBefore(svgTextElement, path.nextSibling);
+    });
+  }
+
   ngOnInit(): void {
+    this.addCountiesAbbreviations();
   }
 
 }
