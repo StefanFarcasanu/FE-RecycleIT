70d435341aa41b758c582832a8629b2872895f12
Livius Habuc <livius.habuc@gmail.com>
2023-01-10 12:58:11 +0200

    Added all pie charts with selected county data

--- frontend-recycle-it/src/app/components/main-view/national-statistics/statistics-dialog/statistics-dialog.component.ts
+++ frontend-recycle-it/src/app/components/main-view/national-statistics/statistics-dialog/statistics-dialog.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatDialogRef} from "@angular/material/dialog";
-import {ChartConfiguration, ChartData, ChartOptions, ChartType} from "chart.js";
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {ChartOptions} from "chart.js";
 
 @Component({
   selector: 'app-statistics-dialog',
@@ -8,18 +8,71 @@ import {ChartConfiguration, ChartData, ChartOptions, ChartType} from "chart.js";
   styleUrls: ['./statistics-dialog.component.css']
 })
 export class StatisticsDialogComponent implements OnInit {
-  public pieChartOptions: ChartOptions<"pie"> = {
+
+  _countyName!: String;
+
+  _countyQuantity!: Number;
+
+  _countyNoVouchers!: Number;
+
+  _countyNoClients!: Number;
+
+  public quantityPieChartOptions: ChartOptions<"pie"> = {
     responsive: true,
     maintainAspectRatio: false
   };
-  public pieChartLabels = ["Selected county", "Other counties"];
-  public pieChartDatasets = [ {
-    data: [100, 200]
-  }];
-  public pieChartLegend = true;
-  public pieChartPlugins = [];
 
-  constructor(private dialogRef: MatDialogRef<StatisticsDialogComponent>) {
+  public noVouchersPieChartOptions: ChartOptions<"pie"> = {
+    responsive: true,
+    maintainAspectRatio: false
+  }
+
+  public noClientsPieChartOptions: ChartOptions<"pie"> = {
+    responsive: true,
+    maintainAspectRatio: false
+  }
+
+  public quantityPieChartLabels = ["Selected county", "Other counties"];
+
+  public noVouchersPieChartLabels = ["Selected county", "Other counties"];
+
+  public noClientsPieChartLabels = ["Selected county", "Other counties"];
+
+  public quantityPieChartDatasets;
+
+  public noVouchersPieChartDatasets;
+
+  public noClientsPieChartDatasets;
+
+  public quantityPieChartLegend = true;
+
+  public noVouchersPieChartLegend = true;
+
+  public noClientsPieChartLegend = true;
+
+  public quantityPieChartPlugins = [];
+
+  public noVouchersPieChartPlugins = [];
+
+  public noClientsPieChartPlugins = [];
+
+  constructor(private dialogRef: MatDialogRef<StatisticsDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public countyData: any) {
+
+    this._countyName = countyData["name"];
+    this._countyQuantity = countyData["quantity"];
+    this._countyNoVouchers = countyData["noVouchers"];
+    this._countyNoClients = countyData["noClients"];
+
+    this.quantityPieChartDatasets = [{
+      data: [this._countyQuantity, 300]
+    }];
+    this.noVouchersPieChartDatasets = [{
+      data: [this._countyNoVouchers, 300]
+    }];
+    this.noClientsPieChartDatasets = [{
+      data: [this._countyNoVouchers, 300]
+    }];
   }
 
   ngOnInit(): void {
